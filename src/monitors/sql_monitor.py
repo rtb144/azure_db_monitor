5e86a48fdac3e4f654c90eb9b791cc76ded3e59e
rtb144 <rtb144@gmail.com>
2024-12-23 13:44:53 -0600

    Update sql_monitor.py

--- src/monitors/sql_monitor.py
+++ src/monitors/sql_monitor.py
@@ -1,1 +1,21 @@
+from .base_monitor import BaseMonitor
+
+class SQLMonitor(BaseMonitor):
+    def __init__(self, subscription_id):
+        super().__init__(subscription_id)
+        self.metrics = [
+            'cpu_percent',
+            'memory_usage_percent',
+            'storage_percent',
+            'dtu_consumption_percent',
+            'connection_successful',
+            'deadlocks'
+        ]
+
+    async def collect_metrics(self, resource_group, server_name):
+        resource_id = (f"/subscriptions/{self.subscription_id}/"
+                      f"resourceGroups/{resource_group}/"
+                      f"providers/Microsoft.Sql/servers/{server_name}")
+        
+        return await self.get_metrics(resource_id, self.metrics)
 
